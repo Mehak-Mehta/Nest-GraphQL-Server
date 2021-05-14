669c5ecb57f4ec1f089024dbb7e881ebdf00686a
Mehak <76558546+Mehak-Mehta@users.noreply.github.com>
2021-05-14 15:42:16 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Text} from './Text'
+
+function App() {
+  return (
+    <div className="App">
+      <Text text =" hi"/>
+    
+    </div>
+  );
+}
+
+export default App;
