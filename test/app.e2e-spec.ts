7792a855a4fc1be4fd691255caad1d2dac14f150
Mehak <76558546+Mehak-Mehta@users.noreply.github.com>
2021-08-25 21:17:49 +0530

    Update app.e2e-spec.ts

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -18,7 +18,7 @@ describe('AppController (e2e)', () => {
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
-      .expect(200)
+      .expect(300)
       .expect('Hello World!');
   });
 });
